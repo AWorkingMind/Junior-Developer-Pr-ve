cf987cad664053d8adc475fa7251f48deabeff54
Christian Rasmussen <chrtobias4780@gmail.com>
2025-03-31 19:13:23 +0200

    Opgave udført

--- chr-assets/chr-java.js
+++ chr-assets/chr-java.js
@@ -1,11 +1,13 @@
-<!-- her begynder jeg at tage fat i nogle id'er jeg har lavet, for at kunne anvende dem og give dem kommandoer.-->
+//her begynder jeg at tage fat i nogle id'er jeg har lavet, for at kunne anvende dem og give dem kommandoer//
     const modal = document.getElementById("myModal");
     const openModalBtn = document.getElementById("openModalBtn");
     const closeModalBtn = document.getElementById("closeModalBtn");
 
-<!-- Her opretter jeg en funktion, der venter på et klik for enten at åbne eller lukke modal'en.
-Man kan lukke modal'en ved at klikke på ikonet, men for en ekstra sikkerhed har jeg også gjort det muligt
-at lukke modal'en ved at klikke uden for modalens indhold. -->
+
+//Her opretter jeg en funktion, der venter på et klik for enten at åbne eller lukke modal'en.Man kan lukke modal'en ved at klikke på ikonet, 
+// men for en ekstra sikkerhed har jeg også gjort det muligtat lukke modal'en ved at klikke uden for 
+// modalens indhold//
+
     openModalBtn.onclick = function() {
     modal.style.display = "block";
 }
